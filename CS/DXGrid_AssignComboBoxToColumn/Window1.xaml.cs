c86aaaace75a8d0e886ba7ce5e38a8a2507c1a26
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_AssignComboBoxToColumn/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Wpf.Grid;
+using System.Data;
+
+namespace DXGrid_AssignComboBoxToColumn {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void TableView_CellValueChanging(object sender, DevExpress.Wpf.Grid.CellValueEventArgs e) {
+            TableView view = sender as TableView;
+            view.PostEditor();
+            this.Title = view.Grid.GetCellValue(e.RowHandle,e.Column.FieldName).ToString();
+        }
+    }
+    public class NwindData {
+        public static nwindDataSet.ProductsDataTable Data {
+            get { return new nwindDataSetTableAdapters.ProductsTableAdapter().GetData(); }
+        }
+    }
+}
---
+++ VB/DXGrid_AssignComboBoxToColumn/Window1.xaml.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Wpf.Grid
+
+Namespace DXGrid_AssignComboBoxToColumn
+    Partial Public Class Window1
+        Inherits Window
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub TableView_CellValueChanging(ByVal sender As Object, ByVal e As DevExpress.Wpf.Grid.CellValueEventArgs)
+            Dim view As TableView = TryCast(sender, TableView)
+            view.PostEditor()
+            Me.Title = view.Grid.GetCellValue(e.RowHandle, e.Column.FieldName).ToString()
+        End Sub
+
+    End Class
+    Public Class NwindData
+        Public Shared ReadOnly Property Data() As nwindDataSet.ProductsDataTable
+            Get
+                Return New nwindDataSetTableAdapters.ProductsTableAdapter().GetData()
+            End Get
+        End Property
+    End Class
+End Namespace
